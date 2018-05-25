355ca12d96781766924cae9bb6a0d3405a9c1e73
eric0315918 <erikkanoh@gmail.com>
2018-05-25 19:17:59 +0900

    test1.rb

---
+++ test1.rb
@@ -0,0 +1,24 @@
+class Measurement
+	def initialize(power_density,watt,volt,max_length,min_length,max_width,min_width)
+		@power_density = power_density
+		@watt = watt
+		@volt = volt
+		@max_length = max_length
+		@min_length = min_length
+		@max_width = max_width
+		@min_width = min_width
+	end
+	def show_value
+		puts @power_density
+		puts @watt
+		puts @volt
+		puts @max_length
+		puts @min_length
+		puts @max_width
+		puts @min_width
+	end
+end
+
+measurement1 = Measurement.new(50,1200,100,60,50,15,10)
+measurement1.show_value
+
